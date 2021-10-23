62e9a608b5ad622dafb1271ffe74d257dad059d1
naumanraees <kingnauman41@gmail.com>
2021-10-23 18:23:26 +0500

    bootstrap setting done dummy carousel set

---
+++ src/components/carousel/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const CarouselTop = (props) => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`${process.env.PUBLIC_URL}/images/img1.png`}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`${process.env.PUBLIC_URL}/images/img2.png`}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`${process.env.PUBLIC_URL}/images/img3.png`}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselTop;
