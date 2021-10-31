607846a0858f1c2f2ab2338e15c9bc86034fb0c4
naumanraees <kingnauman41@gmail.com>
2021-10-31 20:40:57 +0500

    Testimonial set

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import CarouselTop from "./components/carousel";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import TeachingQuran from "./components/teachingQuran";
+import Testimonial from "./components/testimonial";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <CarouselTop />
+      <Testimonial />
       <TeachingQuran />
       <Footer />
     </div>
