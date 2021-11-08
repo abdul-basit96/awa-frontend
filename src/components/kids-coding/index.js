f0609ff044223c4383677649e92d8873cc8547e6
waqarsaikhu <wsaikhou16@gmail.com>
2021-11-08 18:12:08 +0500

    add kids coding section

---
+++ src/components/kids-coding/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import './style.css'
+
+export default function KidsCoding(){
+    return(
+        <>
+        <div class="container">
+        <div class="text-center text-dark mt-4">
+            <h2 class="fw-bold ">Why Coding for Kids?</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mt-4 fw-bold">
+                Put Chart Here
+            </div>
+            <div class="col-md-6 mt-4">
+                <div class="heading">
+                <div class="global-icon start-qoute"></div>
+                <h6>Your kids can learn logic, structure, sequence and creative expression</h6>
+                <div class="global-icon end-qoute"></div>
+                </div>
+                <p class="mt-5">
+                    George Land & Beth Jarman's landmark NASA study identified that kids
+                    
+                    peak in creativity at age 6, followed by a gradual decline as they
+                    
+                    grow up.
+                </p>
+                <p>
+                    The real promise of coding is not that kids will become computer engineers.
+                    
+                    Coding is a fun tool for kids to exercise their desire to build,
+                    
+                    curiosity to question, imagination to explore - the traits that will serve
+                    
+                    any child  well in nearly any career, in every hobby, and in life.
+                </p>
+                <button class="btn0 btn-lg text-light text-align-center">
+                    <i class="far fa-calendar-alt ms-3"></i>
+                    <span class="fw-bold m-3">Book a FREE Trial</span>
+                </button>
+
+            </div>
+        </div>
+    </div>
+
+
+        </>
+    )
+}
