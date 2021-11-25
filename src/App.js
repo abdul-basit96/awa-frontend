a89c5263424cd36ae6d00e1ce25c52a55a4ac4ae
Abdul Basit <abdul.basit@zazmic.com>
2021-11-25 21:27:08 +0500

    remove all old components and now this branch will include quranhost.com website

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import "./App.css";
-import CurriculumPrice from "./components/curriculum_and_pricing";
-import KidsCoding from "./components/kids-coding";
-import ChildLearn from "./components/child-learn-section";
-import SairaVideo from "./components/saira-video-section";
-import JrCarousel from "./components/JrCarousel";
-import ExpertEducators from "./components/expert-educators";
-import TopDiv from './components/top-divs';
-import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <TopDiv />
-      <br />
-      <KidsCoding />
-      <br/>
-      <ChildLearn />
-      <br/>
-      <br/>
-      <JrCarousel/>
-      <br/>
-      <br/>
-      <SairaVideo />
-      <br/>
-      <br/>
-      <CurriculumPrice />
-      <br/>
-      <ExpertEducators />
+      <h1>Website Underdevelopment</h1>
     </div>
   );
 }
