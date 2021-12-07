51603481e2da7ee918c205ecfb232df5461a5e97
naumanraees <kingnauman41@gmail.com>
2021-12-07 23:45:35 +0500

    Footer is being created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./components/common/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Website Underdevelopment</h1>
+      <Footer />
     </div>
   );
 }
