c5955df4c5fc22a039b29d93b7307e0dea57d8b1
Abdul Basit <abdul.basit@zazmic.com>
2021-12-13 11:52:33 +0500

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import HomePage from "./pages/home";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import FeePlan from "./pages/feePlan";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/fee-for-quran-course" element={<FeePlan />} />
+        <Route path="*" element={<Navigate to ="/" />}/>
+      </Routes>
     </div>
   );
 }
