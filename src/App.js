dfbc7a0ee571581d4332c77edad5b1ad4eec6315
naumanraees <kingnauman41@gmail.com>
2021-12-23 00:25:49 +0500

    Merged

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutUs from "./pages/aboutUs";
 import { Routes, Route, Navigate } from "react-router-dom";
 import FeePlan from "./pages/feePlan";
 import ContactPage from "./pages/contact";
+import OnlineQuranReading from "./pages/cources/onlineQuranReading";
 import FaqPage from "./pages/faqpage";
 import TestimonialPage from "./pages/testimonialpage";
 import BlogPage from "./pages/blog";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/fee-for-quran-course" element={<FeePlan />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactPage />} />
+        <Route path="/online-quran-reading" element={<OnlineQuranReading />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/testimonial" element={<TestimonialPage />} />
         <Route path="/blog" element={<BlogPage />} />
