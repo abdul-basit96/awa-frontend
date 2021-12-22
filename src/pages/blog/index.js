ea7a569dd129cfb1b1ed914e3b1167681ad8a725
waqarsaikhu <wsaikhou16@gmail.com>
2021-12-22 18:22:36 +0500

    add blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import './style.css';
-import Header from '../../components/header';
-import Footer from '../../components/common/footer/Footer';
+import React from "react";
+import "./style.css";
+import Header from "../../components/header";
+import Footer from "../../components/common/footer/Footer";
+import { Card } from "react-bootstrap";
 
 const BlogPage = () => {
-    return (
-        <>
-            <Header />
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-12'>
-                            
-                        </div>
-                    </div>
-                </div>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-9"></div>
+          <div className="col-lg-3">
+            <div>
+              <h4 className="fw-bold text-center" style={{ color: "#b5651d" }}>
+                Courses we offer:
+              </h4>
+            </div>
+            <Card className="flex flex-row jss30">
+              <Card.Img
+                className="courseimg"
+                variant="left"
+                src="https://www.quranhost.com/wp-content/uploads/2019/03/noorani-qaidaa-1-105x105.jpg"
+              />
+              <div>
+                <Card.Title
+                  className="m-4 fw-bold text-center"
+                  style={{ fontSize: "14px" }}
+                >
+                  Qaida Noorania Online
+                </Card.Title>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default BlogPage;
