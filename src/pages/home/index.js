3502a02a9a4dfa07299c7d2038ee0beefbc407ce
Abdul Basit <abdul.basit@zazmic.com>
2022-03-05 12:47:19 +0500

    letter section designed

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderTop from "../../components/header/headerTop/headerTop";
+import LetterSection from "../../components/letter-section";
 
 const HomePage = () => {
   return (
     <div>
       <HeaderTop />
+      <LetterSection />
     </div>
   );
 };
