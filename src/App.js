5c4a277c66caad39950324e31ca1d29506b0c4ea
naumanraees <kingnauman41@gmail.com>
2022-03-05 12:26:53 +0500

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import HomePage from "./pages/home";
-import { Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import HomePage from './pages/home';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components/header/headerTop/headerTop.js
+++ src/components/header/headerTop/headerTop.js
@@ -1,79 +1,61 @@
-import React from "react";
-import {
-  Button,
-  Container,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
-import {
-  FaPhoneAlt,
-  FaInstagramSquare,
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-} from "react-icons/fa";
+import React from 'react';
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { FaPhoneAlt, FaInstagramSquare, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
-import { BsYoutube } from "react-icons/bs";
-import { MdOutlineEmail } from "react-icons/md";
-import "./styles.css";
+import { BsYoutube } from 'react-icons/bs';
+import { MdOutlineEmail } from 'react-icons/md';
+import './styles.css';
 const HeaderTop = () => {
-  return (
-    <Navbar expand="lg" className="navbar-top">
-      {/*bg="#ffcc34" */}
-      <Container>
-        <Navbar.Brand href="#">
-          <div className="d-flex flex-column flex-xs-row flex-xxs-row align-items-center">
-            <div className="icon">
-              <FaPhoneAlt size={23} color="white" />
-              <span className="ms-1 me-2 text-white">Call Us Office</span>
-            </div>
-            <div className="text-white">00000000</div>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Brand href="#">
-          <div className="d-flex flex-column align-items-center">
-            <div className="icon">
-              <FaPhoneAlt size={23} color="white" />
-              <span className="ms-1 me-2 text-white">Call UK Office</span>
-            </div>
-            <div className="text-white">00000000</div>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Brand href="#">
-          <div className="d-flex flex-column align-items-center">
-            <div className="icon">
-              <MdOutlineEmail size={23} color="white" />
-              <span className="ms-1 me-2 text-white">Email Us</span>
-            </div>
-            <div className="text-white">arabicwave@gmail.com</div>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto me-3 my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">
-              <FaFacebook size={30} color="black" />
-            </Nav.Link>
-            <Nav.Link href="#action2">
-              <FaInstagramSquare size={30} color="black" />
-            </Nav.Link>
-            <Nav.Link href="#action2">
-              <BsYoutube size={30} color="black" />
-            </Nav.Link>
-            <Nav.Link href="#action2">
-              <FaTwitter size={30} color="black" />
-            </Nav.Link>
-            <Nav.Link href="#action2">
-              <FaLinkedin size={30} color="black" />
-            </Nav.Link>
-            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+	return (
+		<Navbar expand="lg" className="navbar-top px-4">
+			{/*bg="#ffcc34" */}
+			<Container fluid>
+				<Navbar.Brand href="#">
+					<div className="d-flex flex-column flex-xs-row flex-xxs-row align-items-center">
+						<div className="icon">
+							<FaPhoneAlt size={23} color="white" />
+							<span className="ms-1 me-2 text-white">Call Us Office</span>
+						</div>
+						<div className="text-white">00000000</div>
+					</div>
+				</Navbar.Brand>
+				<Navbar.Brand href="#">
+					<div className="d-flex flex-column align-items-center">
+						<div className="icon">
+							<FaPhoneAlt size={23} color="white" />
+							<span className="ms-1 me-2 text-white">Call UK Office</span>
+						</div>
+						<div className="text-white">00000000</div>
+					</div>
+				</Navbar.Brand>
+				<Navbar.Brand href="#">
+					<div className="d-flex flex-column align-items-center">
+						<div className="icon">
+							<MdOutlineEmail size={23} color="white" />
+							<span className="ms-1 me-2 text-white">Email Us</span>
+						</div>
+						<div className="text-white">arabicwave@gmail.com</div>
+					</div>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="navbarScroll" />
+				<Navbar.Collapse id="navbarScroll">
+					<Nav className="ms-auto me-3 my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+						<Nav.Link href="#action1">
+							<FaFacebook size={30} color="black" />
+						</Nav.Link>
+						<Nav.Link href="#action2">
+							<FaInstagramSquare size={30} color="black" />
+						</Nav.Link>
+						<Nav.Link href="#action2">
+							<BsYoutube size={30} color="black" />
+						</Nav.Link>
+						<Nav.Link href="#action2">
+							<FaTwitter size={30} color="black" />
+						</Nav.Link>
+						<Nav.Link href="#action2">
+							<FaLinkedin size={30} color="black" />
+						</Nav.Link>
+						{/* <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -86,8 +68,8 @@ const HeaderTop = () => {
             <Nav.Link href="#" disabled>
               Link
             </Nav.Link> */}
-          </Nav>
-          {/* <Form className="d-flex">
+					</Nav>
+					{/* <Form className="d-flex">
             <FormControl
               type="search"
               placeholder="Search"
@@ -96,16 +78,16 @@ const HeaderTop = () => {
             />
             
           </Form> */}
-          <Button size="lg" className="btn-first text-black bg-white px-4 me-2">
-            Join Class Now
-          </Button>
-          <Button size="lg" className="btn-second px-4">
-            Your Free Trial Class
-          </Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+					<Button size="lg" className="btn-first text-black bg-white px-4 me-2">
+						Join Class Now
+					</Button>
+					<Button size="lg" className="btn-second px-4">
+						Your Free Trial Class
+					</Button>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 };
 
 export default HeaderTop;
